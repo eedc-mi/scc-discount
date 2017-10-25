4aa5bea31ceb7778c886a3fd897c10ec2da9f208
Paul Reid <phreid@ualberta.ca>
2017-10-25 16:03:39 -0600

    merge changes from matrix-sim

--- discountSim.R
+++ discountSim.R
@@ -7,80 +7,160 @@ setwd(here())
 
 tib <- read_rds("data.rds")
 
-proposedDiscountTable <- tibble(
+proposedDiscountMatrix <- cbind(
   fbRev = c(0, 60001, 90001, 105001, 120001, 150001),
   discount = c(0, -.10, -.25, -.35, -.45, -.55)
 )
 
-makeDiscountTable <- function(fbStart, fbStep, dStart, dStep, numSteps) {
-  x = seq(fbStart, (fbStart + fbStep * numSteps), by = fbStep)
-  y = seq(dStart, (dStart + dStep * numSteps), by = dStep)
+makeDiscountMatrix <- function(fbStart, fbStep, dStart, dStep, numSteps) {
+  x = c(0, seq(fbStart, (fbStart + fbStep * numSteps), by = fbStep))
+  y = c(0, seq(dStart, (dStart + dStep * numSteps), by = dStep))
   
-  tib <- rbind(
-    tibble(fbRev = 0, discount = 0),
-    tibble(fbRev = x, discount = y)
-  )
-  
-  return(tib)
+  cbind(x, y)
 }
 
-findDiscount <- function(x, discountTable) {
+findDiscount <- function(x, discountMatrix) {
   if (is.na(x))
     return(NA)
   
-  index <- findInterval(x, discountTable$fbRev)
-  return(as.double(discountTable[index, 2]))
+  index <- findInterval(x, discountMatrix[, 1])
+  as.double(discountMatrix[index, 2])
 }
 
-applyDiscount <- function(data, discountTable) {
-  return(
-    data %>%
-      rowwise() %>%
-      mutate(
-        new_discount = rental_revenue * findDiscount(food_beverage_revenue, discountTable),
-        new_rental_revenue = rental_revenue + new_discount
-      ) %>%
-      group_by(type) %>%
-      summarize(
-        new_rental_revenue = sum(new_rental_revenue, na.rm = TRUE)
+applyDiscount <- function(dataMatrix, discountMatrix) {
+  dataMatrix <- cbind(
+    dataMatrix, 
+    new_discount = as.numeric(
+      map(
+        dataMatrix[, "food_beverage_revenue"], 
+        findDiscount, 
+        discountMatrix = discountMatrix
       )
+    )
   )
+  
+  dataMatrix[, c("event_id", "new_discount")]
 }
 
 simStartConditions <- function(data, minRevSeq, minDiscountSeq, fbStep, dStep, numSteps) {
   grid <- expand.grid(rev = minRevSeq, disc = minDiscountSeq)
   
-  dTableList <- mapply(
-    makeDiscountTable, 
+  dMatrixList <- mapply(
+    makeDiscountMatrix, 
     fbStart = grid$rev, 
     dStart = grid$disc, 
     MoreArgs = list(fbStep = fbStep, dStep = dStep, numSteps = numSteps),
     SIMPLIFY = FALSE
   )
   
-  tibList <- lapply(dTableList, applyDiscount, data = data)
+  dataMatrix <- data.matrix(
+    data %>% select(event_id, rental_revenue, food_beverage_revenue)
+  )
   
-  return(
-    as.tibble(
-      cbind(
-        grid[rep(seq_len(nrow(grid)), each = nrow(tibList[[1]])),],
-        bind_rows(tibList)
+  matrixList <- lapply(dMatrixList, applyDiscount, dataMatrix = dataMatrix)
+  
+  tib <- as.tibble(
+    cbind(
+      grid[rep(seq_len(nrow(grid)), each = nrow(matrixList[[1]])),],
+      do.call("rbind", matrixList)
+    )
+  )
+  
+  data %>%
+    left_join(tib, by = "event_id") %>%
+    mutate(new_rental_revenue = rental_revenue + (rental_revenue * new_discount)) %>%
+    group_by(rev, disc, type) %>%
+    summarize(new_rental_revenue = sum(new_rental_revenue, na.rm = TRUE))
+             
+}
+
+simStepConditions <- function(data, fbStart, dStart, fbStepSeq, dStepSeq, numSteps) {
+  grid <- expand.grid(rev = fbStepSeq, disc = dStepSeq)
+  
+  dMatrixList <- mapply(
+    makeDiscountMatrix, 
+    fbStep = grid$rev, 
+    dStep = grid$disc, 
+    MoreArgs = list(fbStart = fbStart, dStart = dStart, numSteps = numSteps),
+    SIMPLIFY = FALSE
+  )
+  
+  dataMatrix <- data.matrix(
+    data %>% select(event_id, rental_revenue, food_beverage_revenue)
+  )
+  
+  matrixList <- lapply(dMatrixList, applyDiscount, dataMatrix = dataMatrix)
+  
+  tib <- as.tibble(
+    cbind(
+      grid[rep(seq_len(nrow(grid)), each = nrow(matrixList[[1]])),],
+      do.call("rbind", matrixList)
+    )
+  )
+  
+  data %>%
+    left_join(tib, by = "event_id") %>%
+    mutate(new_rental_revenue = rental_revenue + (rental_revenue * new_discount)) %>%
+    group_by(rev, disc, type) %>%
+    summarize(new_rental_revenue = sum(new_rental_revenue, na.rm = TRUE))
+  
+}
+
+proposedDiscResult <- as.tibble(
+  applyDiscount(
+    data.matrix(
+      tib %>% select(event_id, rental_revenue, food_beverage_revenue)
+    ), 
+    proposedDiscountTable
+  )
+)
+
+proposedDiscResult <- tib %>%
+  left_join(proposedDiscResult, by = "event_id") %>%
+  mutate(
+    new_rental_revenue = rental_revenue + (rental_revenue * new_discount),
+    old_rental_revenue = rental_revenue + rental_discount
+  ) %>%
+  group_by(type) %>%
+  summarize(
+    new_rental_revenue = sum(new_rental_revenue, na.rm = TRUE),
+    old_rental_revenue = sum(old_rental_revenue, na.rm = TRUE)
+  )
+
+minFbRev <- seq(30000, 100000, by = 1000)
+minDisc <- seq(-0.1, -.30, by = -0.05)
+sim <- simStartConditions(tib, minFbRev, minDisc, 30000, -0.1, 5)
+
+stepSeq <- seq(5000, 50000, by = 1000)
+discSeq <- seq(-.05, -.30, by = -0.05)
+simStep <- simStepConditions(tib, 30000, -.10, fbStepSeq, discStepSeq, 5)
+
+ggplot(filter(sim, type == "Convention (CONV)"), aes(x = rev, y = new_rental_revenue, color = factor(disc))) +
+  geom_line() + 
+  geom_hline(
+    yintercept = c(
+      first(
+        proposedDiscResult %>% filter(type == "Convention (CONV)") %>% select(new_rental_revenue)
+      ),
+      first(
+        proposedDiscResult %>% filter(type == "Convention (CONV)") %>% select(old_rental_revenue)
+      )
+    ), 
+    color = c("red", "blue")
+  )
+ 
+ggplot(filter(simStep, type == "Convention (CONV)"), aes(x = rev, y = new_rental_revenue, color = factor(disc))) +
+  geom_line() +
+  geom_hline(
+    yintercept = c(
+      first(
+        proposedDiscResult %>% filter(type == "Convention (CONV)") %>% select(new_rental_revenue)
+      ),
+      first(
+        proposedDiscResult %>% filter(type == "Convention (CONV)") %>% select(old_rental_revenue)
       )
     )
   )
-}
-
-minFbRev <- seq(0, 100000, by = 1000)
-minDisc <- seq(-0.1, -.50, by = -0.1)
-
-sim <- simStartConditions(tib, minFbRev, minDisc, 30000, -0.1, 5)
-
-ggplot(filter(sim, type == "Convention (CONV)"), aes(x = rev, y = new_rental_revenue, color = factor(disc))) +
-  geom_line()
-
-proposedDiscountResult <- applyDiscount(tib, proposedDiscountTable)
-
-
 
 
 
