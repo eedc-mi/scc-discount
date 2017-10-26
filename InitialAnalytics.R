87bb10ed878335dac4e404d5b57781f3e54be1c2
Paul Reid <phreid@ualberta.ca>
2017-10-26 11:28:20 -0600

    merge kristen's changes

--- InitialAnalytics.R
+++ InitialAnalytics.R
@@ -33,6 +33,10 @@ d2 <- d2[!(d2$rental_revenue == 0),]
 
 d2$rental_discount <- abs(d2$rental_discount)
 
+# Omit values of zero in the total_event_attendance column
+
+d2 <- d2[!(d2$total_event_attendance == 0),]
+
 # Create bins for total_event_attendence
 
 d2$tea_bins <- cut(d2$total_event_attendance, c(0,5000,10000,15000,20000,25000,
@@ -56,6 +60,10 @@ month_booked <- months(d2$date_booked)
 d2$event_month <- event_month
 d2$month_booked <- month_booked
 
+# Add year_booked column
+
+d2$year_booked <- year(d2$date_booked)
+
 # Add advance_booking column (days booked in advance of event)
 
 d2$advance_booking <- as.numeric(d2$start_date - d2$date_booked)
@@ -65,6 +73,16 @@ d2$ab_bins <- cut(d2$advance_booking, c(0,100,200,300,400,500,600,700,800,900))
 # Add number_of_days column (number of days of the event)
 
 d2$number_of_days <- as.numeric(d2$end_date - d2$start_date + 1)
+
+# Add column that displays count of booked_spaces
+
+dtest <- d2
+dtest2 <- unnest(dtest, booked_spaces)
+
+dcount <- aggregate(data.frame(count = dtest2$event_id),
+                    list(value = dtest2$event_id), length)
+
+d2$space_count <- dcount$count
 
 # Full Sample Stats
 #-------------------
@@ -79,7 +97,8 @@ max(d2$rental_discount)
 #--------------
 
 # Rental Discount by type, total event attendance, advanced booking,
-# total revenue, number of days of the event, event_month, and month_booked
+# total revenue, number of days of the event, number of spaces booked,
+# event_month, and month_booked
 
 d2 %>%
   group_by(type) %>%
@@ -119,6 +138,24 @@ d2 %>%
 
 d2 %>%
   group_by(number_of_days) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n())
+
+d2 %>%
+  group_by(space_count) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked) %>%
   summarise(mean = mean(rental_discount),
             median = median(rental_discount),
             standard_dev = sd(rental_discount),
@@ -157,7 +194,8 @@ d2 %>%
   arrange(month_booked)
 
 # Percentage Discount by type, total event attendance, advanced booking,
-# total revenue, number of days of the event, event_month, and month_booked
+# total revenue, number of days of the event, number of spaces booked,
+# event_month, and month_booked
 
 d2 %>%
   group_by(type) %>%
@@ -205,6 +243,24 @@ d2 %>%
             n = n())
 
 d2 %>%
+  group_by(space_count) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n())
+
+d2 %>%
   group_by(event_month) %>%
   summarise(mean = mean(percentage_discount),
             median = median(percentage_discount),
@@ -233,6 +289,183 @@ d2 %>%
                                           "September", "October", "November", 
                                           "December"))) %>%
   arrange(month_booked)
+
+# Rental Discount by year_booked and type, total event attendance, advanced booking,
+# total revenue, number of days of the event, number of spaces booked,
+# event_month, and month_booked
+
+d2 %>%
+  group_by(year_booked, type) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, tea_bins) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, ab_bins) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, tr_bins) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, number_of_days) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, space_count) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, event_month) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n()) %>%
+  mutate(event_month = factor(event_month,
+                              levels = c("January", "February", "March", "April",
+                                         "May", "June", "July", "August",
+                                         "September", "October", "November", 
+                                         "December"))) %>%
+  arrange(event_month)
+
+d2 %>%
+  group_by(year_booked, month_booked) %>%
+  summarise(mean = mean(rental_discount),
+            median = median(rental_discount),
+            standard_dev = sd(rental_discount),
+            min = min(rental_discount),
+            max = max(rental_discount),
+            n = n()) %>%
+  mutate(month_booked = factor(month_booked,
+                               levels = c("January", "February", "March", "April",
+                                          "May", "June", "July", "August",
+                                          "September", "October", "November", 
+                                          "December"))) %>%
+  arrange(month_booked)
+
+# Percentage Discount by year_booked and type, total event attendance, advanced booking,
+# total revenue, number of days of the event, number of spaces booked,
+# event_month, and month_booked
+
+d2 %>%
+  group_by(year_booked, type) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, tea_bins) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, ab_bins) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, tr_bins) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, number_of_days) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, space_count) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n())
+
+d2 %>%
+  group_by(year_booked, event_month) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n()) %>%
+  mutate(event_month = factor(event_month,
+                              levels = c("January", "February", "March", "April",
+                                         "May", "June", "July", "August",
+                                         "September", "October", "November", 
+                                         "December"))) %>%
+  arrange(event_month)
+
+comparison_byYearandMonthBooked <- d2 %>%
+  group_by(year_booked, month_booked) %>%
+  summarise(mean = mean(percentage_discount),
+            median = median(percentage_discount),
+            standard_dev = sd(percentage_discount),
+            min = min(percentage_discount),
+            max = max(percentage_discount),
+            n = n()) %>%
+  mutate(month_booked = factor(month_booked,
+                               levels = c("January", "February", "March", "April",
+                                          "May", "June", "July", "August",
+                                          "September", "October", "November", 
+                                          "December"))) %>%
+  arrange(month_booked)
+
 
 
 
