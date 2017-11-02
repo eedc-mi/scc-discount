d4b5e81f9ca99b34d44a4271b4e2566856910306
Paul Reid <phreid@ualberta.ca>
2017-11-02 11:53:04 -0600

    bar chart

--- discountSim.R
+++ discountSim.R
@@ -115,8 +115,10 @@ proposedDiscResult <- as.tibble(
   )
 )
 
-proposedDiscResult <- tib %>%
-  left_join(proposedDiscResult, by = "event_id") %>%
+proposedDiscAll <- tib %>%
+  left_join(proposedDiscResult, by = "event_id")
+
+proposedDiscResult <- proposedDiscAll %>%
   mutate(
     new_rental_revenue = rental_revenue + (rental_revenue * new_discount),
     old_rental_revenue = rental_revenue + rental_discount
@@ -225,5 +227,21 @@ ggplot(
     subtitle = "Fraction of Jan 2015 - Jun 2017 events with given amounts of food and beverage revenue"
   ) +
   scale_x_continuous(limits = c(0, 200000), labels = scales::comma)
+
+ggplot(
+  proposedDiscAll %>%
+    group_by(type) %>%
+    summarize(
+      count_total = n(),
+      count_old = sum(rental_discount < 0, na.rm = TRUE),
+      count_new = sum(new_discount < 0, na.rm = TRUE)
+    ) %>%
+    mutate(percent_old = count_old / count_total, percent_new = count_new / count_total) %>%
+    select(type, percent_old, percent_new) %>%
+    filter(percent_old > 0) %>%
+    gather(key = percent, value = value, percent_old:percent_new),
+  aes(x = type, y = value, fill = percent)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  coord_flip()
 
 
